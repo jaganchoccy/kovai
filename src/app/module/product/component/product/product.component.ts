cd47b30740aad528bdeece505bb5576027feea4d
jaganchoccy <jagan.ramanikanth@gmail.com>
2023-11-07 21:17:15 +0530

    align

--- src/app/module/product/component/product/product.component.ts
+++ src/app/module/product/component/product/product.component.ts
@@ -7,19 +7,20 @@ import { ThemeService } from 'src/app/shared/theme.service';
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
-  
 })
 export class ProductComponent implements OnInit {
-  
   isLoading = true;
   public products: Product | any = [];
-  public productCategories :ProductCategory | any = [];
+  public productCategories: ProductCategory | any = [];
   showDescription = false;
-  selectedOption : number = 0;
+  selectedOption: number = 0;
   cacheProduct: Product | any = [];
-  
+
   searchQuery: string = '';
-  constructor(private productService: ProductService, public themeService: ThemeService) {}
+  constructor(
+    private productService: ProductService,
+    public themeService: ThemeService
+  ) {}
 
   ngOnInit(): void {
     this.GetProducts();
@@ -30,33 +31,34 @@ export class ProductComponent implements OnInit {
     this.themeService.toggleTheme();
   }
 
-  RandamRate(){
-    return Math.floor(Math.random() * 900) + 100; 
+  RandamRate() {
+    return Math.floor(Math.random() * 900) + 100;
   }
 
   onSearch() {
     var filteredProducts = [];
-    filteredProducts = this.products.filter((p:Product)=>{
-       return p.productName.toLowerCase().includes(this.searchQuery.toLowerCase())
-      });
+    filteredProducts = this.products.filter((p: Product) => {
+      return p.productName
+        .toLowerCase()
+        .includes(this.searchQuery.toLowerCase());
+    });
 
-      console.log(filteredProducts);
+    console.log(filteredProducts);
     this.products = filteredProducts;
   }
 
-  clear(){
-    this.searchQuery = "";
+  clear() {
+    this.searchQuery = '';
     this.GetProducts();
   }
 
-  getProductById(){
-    if(this.selectedOption == 0) {
+  getProductById() {
+    if (this.selectedOption == 0) {
       this.GetProducts();
-    }else{
+    } else {
       this.productService.GetProductById(this.selectedOption).subscribe({
         next: (res: any) => {
-          
-          if(res && res.result){
+          if (res && res.result) {
             this.products = res?.data;
             this.isLoading = false;
           }
@@ -70,48 +72,36 @@ export class ProductComponent implements OnInit {
         },
       });
     }
-  
   }
-  
 
-  
   private GetAllCategory() {
     this.productService.GetproductCategory().subscribe({
-      
       next: (res: any) => {
-        
-        if(res && res.result){
+        if (res && res.result) {
           this.productCategories = res?.data;
 
-          let label= {
-   
-              "parentCategoryId" :  0,
-              categoryName:         "Categories",
-              categoryId:         0
-          
-          }
+          let label = {
+            parentCategoryId: 0,
+            categoryName: 'Categories',
+            categoryId: 0,
+          };
 
-          this.productCategories.unshift(label)
-          
+          this.productCategories.unshift(label);
         }
       },
       error: (e) => {
         console.error(e);
-       
       },
       complete() {
-        
         console.log('is completed');
       },
     });
   }
 
-
-
   private GetProducts() {
     this.productService.GetProducts().subscribe({
       next: (res: any) => {
-        if(res && res.result){
+        if (res && res.result) {
           this.products = res?.data;
           this.isLoading = false;
         }
